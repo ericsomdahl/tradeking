ae0f9e04b94f6fd2f1ec1e48ebac2ea2571e0d2e
Eric Somdahl <ericsomdahl@gmail.com>
2018-09-03 14:45:26 -0400

    configuration

--- setup.py
+++ setup.py
@@ -4,10 +4,17 @@ from distutils.core import setup
 
 setup(name='Tradeking',
       version='0.1-dev',
-      packages=['tradeking',],
-      
+      packages=['tradeking', ],
       description='TradeKing API Calls',
-      author='Jeff Gregory',
-      author_email='jeffmgreg@gmail.com',
-      
-     )
+      author='Jeff Gregory, Eric Somdahl',
+      author_email='jeffmgreg@gmail.com, eric@corsairconsulting.com',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: MIT License',
+          'Development Status :: 3 - Alpha',
+          'Topic :: Office/Business :: Financial',
+      ]
+      )
