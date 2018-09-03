abf1eb525fe6543d76165ca16130e0efbaea1e2b
Eric Somdahl <ericsomdahl@gmail.com>
2018-09-03 15:09:07 -0400

    will make this work

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Tradeking',
-      version='0.1-dev',
+      version='0.1',
       packages=['tradeking', ],
       description='TradeKing API Calls',
       author='Jeff Gregory, Eric Somdahl',
